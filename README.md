45005b357ac8cdcc8918705fd54f1f3a55df9e17
Will Binns <will@trek.io>
2016-11-14 21:27:14 -0600

    repo: Initialize repository

---
+++ README.md
@@ -0,0 +1,8 @@
+# Bitcoin API Template
+
+How to take an existing API and wrap it so that requests to it pay you bitcoin.
+
+## Requirements
+
+- 21 [https://21.co/signup]
+- An API to wrap.
---
+++ index.py
@@ -0,0 +1,52 @@
+#     ____  _ __             _          ___    ____  ____
+#    / __ )(_) /__________  (_)___     /   |  / __ \/  _/
+#   / __  / / __/ ___/ __ \/ / __ \   / /| | / /_/ // /  
+#  / /_/ / / /_/ /__/ /_/ / / / / /  / ___ |/ ____// /   
+# /_____/_/\__/\___/\____/_/_/ /_/  /_/  |_/_/   /___/   
+                                                       
+#   ______                     __      __     
+#  /_  __/__  ____ ___  ____  / /___ _/ /____ 
+#   / / / _ \/ __ `__ \/ __ \/ / __ `/ __/ _ \
+#  / / /  __/ / / / / / /_/ / / /_/ / /_/  __/
+# /_/  \___/_/ /_/ /_/ .___/_/\__,_/\__/\___/ 
+#                  /_/                       
+
+# Description: How to take an API and wrap it so requests to it pay you bitcoin.
+# Repo: Bitcoin API Template (github.com/cryptopelago/bitcoin-api-template)
+# License: Unlicense (unlicense.org)
+
+# Load libraries
+import requests
+import urllib
+import json
+import yaml
+from flask import Flask, request
+from two1.wallet import Wallet
+from two1.bitserv.flask import Payment
+
+# Init Flask, Wallet and Payment
+app = Flask(__name__)
+wallet = Wallet()
+payment = Payment(app, wallet)
+
+# Add 402
+@app.route('/your_route')
+@payment.required(your_price)
+def lookup_string():
+    something = request.args.get('something')
+    link = requests.get('some_url'+something)
+    return link.text
+
+# Add Manifest
+@app.route('/manifest')
+def docs():
+    '''
+    Serves the app manifest to the 21 crawler.
+    '''
+    with open('manifest.yaml', 'r') as f:
+        manifest_yaml = yaml.load(f)
+    return json.dumps(manifest_yaml)
+
+# Init Host
+if __name__=='__main__':
+    app.run(host='::', port='your_port')
